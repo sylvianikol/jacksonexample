05cdc39b4085be72be8474840aa42f83efc86d44
sylvianikol <silviaadvert@gmail.com>
2021-06-11 09:31:21 +0300

    added Map From JSON String example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -17,6 +18,7 @@ public class Main {
         String carAsString = objectMapper.writeValueAsString(car);
 
         System.out.println(carAsString);
+        System.out.println("-".repeat(20));
 
         String jsonBmw = "{ \"color\" : \"Black\", \"type\" : \"BMW\" }";
         Car carFromJson = objectMapper.readValue(jsonBmw, Car.class);
@@ -24,17 +26,25 @@ public class Main {
         Car carFromUrl = objectMapper.readValue(new URL("file:src/main/resources/files/car.json"), Car.class);
 
         System.out.println(carFromUrl);
+        System.out.println("-".repeat(20));
 
         String jsonFiat = "{ \"color\" : \"Black\", \"type\" : \"Fiat\" }";
         JsonNode jsonNode = objectMapper.readTree(jsonFiat);
         String color = jsonNode.get("color").asText();
 
         System.out.println(color);
+        System.out.println("-".repeat(20));
 
         String jsonCarArray =
                 "[{ \"color\" : \"Black\", \"type\" : \"BMW\" }, { \"color\" : \"Red\", \"type\" : \"FIAT\" }]";
         List<Car> cars = objectMapper.readValue(jsonCarArray, new TypeReference<List<Car>>() {});
 
         cars.forEach(System.out::println);
+        System.out.println("-".repeat(20));
+
+        String jsonMap = "{ \"color\" : \"Black\", \"type\" : \"BMW\" }";
+        Map<String, Object> map = objectMapper.readValue(jsonMap, new TypeReference<Map<String, Object>>() {});
+
+        map.forEach((key, value) -> System.out.println(key + " - " + value));
     }
 }
