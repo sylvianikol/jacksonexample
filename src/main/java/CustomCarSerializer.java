f95ec4c1830e9d6824f95e77fa088c36027ac1fe
sylvianikol <silviaadvert@gmail.com>
2021-06-11 09:48:35 +0300

    added custom JSON serializer example

---
+++ src/main/java/CustomCarSerializer.java
@@ -0,0 +1,23 @@
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+
+public class CustomCarSerializer extends StdSerializer<Car> {
+
+    public CustomCarSerializer() {
+        this(null);
+    }
+
+    public CustomCarSerializer(Class<Car> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(Car car, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeStringField("car_brand", car.getType());
+        jsonGenerator.writeEndObject();
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
+import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,7 +21,7 @@ public class Main {
         String carAsString = objectMapper.writeValueAsString(car);
 
         System.out.println(carAsString);
-        System.out.println("-".repeat(20));
+        delimiter();
 
         String jsonBmw = "{ \"color\" : \"Black\", \"type\" : \"BMW\" }";
         Car carFromJson = objectMapper.readValue(jsonBmw, Car.class);
@@ -27,27 +29,27 @@ public class Main {
         Car carFromUrl = objectMapper.readValue(new URL("file:src/main/resources/files/car.json"), Car.class);
 
         System.out.println(carFromUrl);
-        System.out.println("-".repeat(20));
+        delimiter();
 
         String jsonFiat = "{ \"color\" : \"Black\", \"type\" : \"Fiat\" }";
         JsonNode jsonNode = objectMapper.readTree(jsonFiat);
         String color = jsonNode.get("color").asText();
 
         System.out.println(color);
-        System.out.println("-".repeat(20));
+        delimiter();
 
         String jsonCarArray =
                 "[{ \"color\" : \"Black\", \"type\" : \"BMW\" }, { \"color\" : \"Red\", \"type\" : \"FIAT\" }]";
         List<Car> cars = objectMapper.readValue(jsonCarArray, new TypeReference<List<Car>>() {});
 
         cars.forEach(System.out::println);
-        System.out.println("-".repeat(20));
+        delimiter();
 
         String jsonMap = "{ \"color\" : \"Black\", \"type\" : \"BMW\" }";
         Map<String, Object> map = objectMapper.readValue(jsonMap, new TypeReference<Map<String, Object>>() {});
 
         map.forEach((key, value) -> System.out.println(key + " - " + value));
-        System.out.println("-".repeat(20));
+        delimiter();
 
         String jsonString = "{ \"color\" : \"Black\", \"type\" : \"Fiat\", \"year\" : \"1970\" }";
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
@@ -58,6 +60,20 @@ public class Main {
         String year = jsonNodeYear.asText();
 
         System.out.println(year);
+        delimiter();
+
+        SimpleModule module = new SimpleModule("CustomCarSerializer",
+                new Version(1, 0, 0, null, null, null));
+        module.addSerializer(Car.class, new CustomCarSerializer());
+        objectMapper.registerModule(module);
+        Car car2 = new Car("yellow", "renault");
+        String car2Json = objectMapper.writeValueAsString(car2);
+
+        System.out.println(car2Json);
+        delimiter();
+    }
+
+    private static void delimiter() {
         System.out.println("-".repeat(20));
     }
 }
