f15273309c66880fb668df68fe7b1bd9d46b7bc5
sylvianikol <silviaadvert@gmail.com>
2021-06-13 09:35:39 +0300

    added @JsonIgnoreProperties example and test

---
+++ src/test/java/BeanWithIgnoreTest.java
@@ -0,0 +1,22 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+
+class BeanWithIgnoreTest {
+
+    @Test
+    void test_serializeUsingJsonIgnoreProperties() throws JsonProcessingException {
+
+        BeanWithIgnore bean = new BeanWithIgnore(1, "My bean");
+
+        String result = new ObjectMapper().writeValueAsString(bean);
+
+        assertThat(result, containsString("My bean"));
+        assertThat(result, not(containsString("id")));
+    }
+}
