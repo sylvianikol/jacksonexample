53417f50eef5238d02ab38e135c90db3ffdbff24
sylvianikol <silviaadvert@gmail.com>
2021-06-13 09:03:12 +0300

    added @JacksonInject example and test

---
+++ src/main/java/BeanWithInject.java
@@ -0,0 +1,32 @@
+import com.fasterxml.jackson.annotation.JacksonInject;
+
+public class BeanWithInject {
+
+    @JacksonInject
+    private int id;
+    private String name;
+
+    public BeanWithInject() {
+    }
+
+    public BeanWithInject(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
