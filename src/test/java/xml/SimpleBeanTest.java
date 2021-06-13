c52bd426f3f1979fa3fb59b88f50695fcb4976e8
sylvianikol <silviaadvert@gmail.com>
2021-06-13 11:33:15 +0300

    added serialize to XML file example and test

--- src/test/java/xml/SimpleBeanTest.java
+++ src/test/java/xml/SimpleBeanTest.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleBeanTest {
@@ -14,5 +17,15 @@ class SimpleBeanTest {
         XmlMapper mapper = new XmlMapper();
         String xml = mapper.writeValueAsString(new SimpleBean(1, 2));
         assertNotNull(xml);
+    }
+
+    @Test
+    void test_serializeToXmlFile() throws IOException {
+
+        XmlMapper mapper = new XmlMapper();
+        mapper.writeValue(new File("src/test/resources/files/output.xml"), new SimpleBean(1, 2));
+
+        File file = new File("src/test/resources/files/output.xml");
+        assertNotNull(file);
     }
 }
