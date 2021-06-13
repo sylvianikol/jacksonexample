eabec284cd3caa790d55b4bb5ad9ff1d5e1b8e50
sylvianikol <silviaadvert@gmail.com>
2021-06-13 11:42:08 +0300

    added deserialize from XML file example and test

--- src/test/java/xml/SimpleBeanTest.java
+++ src/test/java/xml/SimpleBeanTest.java
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,5 +35,32 @@ class SimpleBeanTest {
         SimpleBean bean = mapper.readValue("<SimpleBean><x>1</x><y>2</y></SimpleBean>", SimpleBean.class);
 
         assertTrue(bean.getX() == 1 && bean.getY() == 2);
+    }
+
+    @Test
+    void test_deserializeFromXmlFile() throws IOException {
+
+        File file = new File("src/test/resources/files/output.xml");
+        XmlMapper mapper = new XmlMapper();
+
+        String xml = inputStreamToString(new FileInputStream(file));
+
+        SimpleBean bean = mapper.readValue(xml, SimpleBean.class);
+        assertTrue(bean.getX() == 1 && bean.getY() == 2);
+    }
+
+    private String inputStreamToString(InputStream is) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+
+        reader.close();
+
+        return sb.toString();
     }
 }
