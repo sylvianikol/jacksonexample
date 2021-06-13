1948753bd386afc6db8a503bac1e781cdded1563
sylvianikol <silviaadvert@gmail.com>
2021-06-13 11:06:06 +0300

    added @JsonFilter example and test

---
+++ src/main/java/BeanWithFilter.java
@@ -0,0 +1,32 @@
+import com.fasterxml.jackson.annotation.JsonFilter;
+
+@JsonFilter("myFilter")
+public class BeanWithFilter {
+
+    private int id;
+    private String name;
+
+    public BeanWithFilter() {
+    }
+
+    public BeanWithFilter(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
