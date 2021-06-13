ff5fc5e14d02e85ff7459d48ed97870ebbcba208
sylvianikol <silviaadvert@gmail.com>
2021-06-13 08:29:04 +0300

    added @JsonRawValue example and test

---
+++ src/main/java/RawBean.java
@@ -0,0 +1,32 @@
+import com.fasterxml.jackson.annotation.JsonRawValue;
+
+public class RawBean {
+
+    private String name;
+    @JsonRawValue
+    private String json;
+
+    public RawBean() {
+    }
+
+    public RawBean(String name, String json) {
+        this.name = name;
+        this.json = json;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+}
