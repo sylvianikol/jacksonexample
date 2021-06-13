b54788345a7a82e81c67b428eaea31112b63a13f
sylvianikol <silviaadvert@gmail.com>
2021-06-13 09:51:45 +0300

    added @JsonAutoDetect example and test

---
+++ src/test/java/PrivateBeanTest.java
@@ -0,0 +1,21 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class PrivateBeanTest {
+
+    @Test
+    void test_serializeUsingJsonAutoDetect() throws JsonProcessingException {
+
+        PrivateBean bean = new PrivateBean(1, "My bean");
+
+        String result = new ObjectMapper()
+                .writeValueAsString(bean);
+
+        assertThat(result, containsString("1"));
+        assertThat(result, containsString("My bean"));
+    }
+}
